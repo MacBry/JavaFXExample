1dc6b8a44894347054a42dd463e36bb76115ddfd
MacBry <maciej.bryja@wp.pl>
2021-03-09 14:06:03 +0100

    Add lifecycle methods

--- src/main/java/com/mac/bry/fx/FirstWindow.java
+++ src/main/java/com/mac/bry/fx/FirstWindow.java
@@ -8,21 +8,31 @@ import javafx.stage.Stage;
 public class FirstWindow extends Application {
 
 	public static void main(String[] args) {
+		System.out.println("Main method");
 		Application.launch();
 
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+		System.out.println("Start method");
 		StackPane pane = new StackPane();
 		Label label = new Label("First Window");
 		Scene scene = new Scene(pane,300,300);
 		pane.getChildren().add(label);
-		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
+	}
+	
+	@Override
+	public void init() {
+		System.out.println("Init method");
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Stop method");
 	}
 
 }
